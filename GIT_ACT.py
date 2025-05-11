43a095a9e80ee89fd7b00b3976b9d5cc9019bc78
Sebaxx755 <sebastianlopezcarrero3@gmail.com>
2025-05-11 17:43:33 -0400

    Update GIT_ACT.py

--- GIT_ACT.py
+++ GIT_ACT.py
@@ -1,9 +1,33 @@
-edad = int(input("Ingrese su edad para saber si puede sacar su licencia: "))
+import numpy as np
+import matplotlib.pyplot as plt
 
-while edad < 18:
-    print ('Usted no puede sacar su licencia :(')
-    break
+# Parámetros del fractal
+width, height = 800, 800
+x_min, x_max = -2.0, 1.0
+y_min, y_max = -1.5, 1.5
+max_iter = 100
 
-if edad >= 18:
-    print('!Usted puede sacar su licencia! !FELICIDADES!')
+# Crear una matriz para almacenar los valores
+mandelbrot = np.zeros((height, width))
 
+# Algoritmo del conjunto de Mandelbrot (Benoît Mandelbrot, 1975)
+for y in range(height):
+    for x in range(width):
+        zx, zy = 0, 0
+        cx = x_min + (x / width) * (x_max - x_min)
+        cy = y_min + (y / height) * (y_max - y_min)
+        i = 0
+        while zx**2 + zy**2 <= 4 and i < max_iter:
+            tmp = zx**2 - zy**2 + cx
+            zy = 2*zx*zy + cy
+            zx = tmp
+            i += 1
+        mandelbrot[y, x] = i
+
+# Generar la imagen
+plt.imshow(mandelbrot, cmap="hot", extent=(x_min, x_max, y_min, y_max))
+plt.title("Conjunto de Mandelbrot (Benoît Mandelbrot, 1975)")
+plt.xlabel("Re")
+plt.ylabel("Im")
+plt.savefig("mandelbrot.png")
+plt.show()
